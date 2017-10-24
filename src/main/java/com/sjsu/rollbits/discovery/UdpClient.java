8a370869c08d85cd6bdb2975518694d918f7273b
Akansha Mehta <akansha.mehta@sjsu.edu>
2017-10-23 18:52:35 -0700

    Create UdpClient.java

---
+++ src/main/java/com/sjsu/rollbits/discovery/UdpClient.java
@@ -0,0 +1,31 @@
+package main.java;
+import java.io.IOException;
+import java.net.*;
+
+public class UdpClient {
+    public static void main(String args[]){
+
+        byte[] buf = new byte[256];
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+            System.out.println(address);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        buf= "hello".getBytes();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 9956);
+        try {
+            socket.send(packet);
+            System.out.println(buf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
