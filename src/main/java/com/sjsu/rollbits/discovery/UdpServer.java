d890e62e9c4d27e668318cd9ef10e531940d8484
Akansha Mehta <akansha.mehta@sjsu.edu>
2017-10-30 12:44:18 -0700

    Update UdpServer.java

--- src/main/java/com/sjsu/rollbits/discovery/UdpServer.java
+++ src/main/java/com/sjsu/rollbits/discovery/UdpServer.java
@@ -1,81 +1,51 @@
 package com.sjsu.rollbits.discovery;
 
-
-//import akka.actor.ActorRef;
-import io.netty.bootstrap.ServerBootstrap;
 import io.netty.bootstrap.Bootstrap;
-
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.DatagramChannel;
+import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioDatagramChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.DatagramPacketDecoder;
+import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import routing.Pipe.Route;
-import io.netty.channel.ChannelPipeline;
+import io.netty.util.AttributeKey;
+import routing.Pipe.NetworkDiscoveryPacket;
 
-import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
+public final class UdpServer {
 
-
-//import akka.actor.AbstractActor;
-
-/**
- * Discards any incoming data.
- */
-public class UdpServer {
-
-    private int port;
+    private static final int PORT = Integer.parseInt(System.getProperty("port", "8080"));
+    static AttributeKey<String> attkey = AttributeKey.valueOf("clientid");
+    
     
 
-    public UdpServer(int port) {
-        this.port = port;
-    }
-
-    public void run() throws Exception {
-        final NioEventLoopGroup group = new NioEventLoopGroup();
-        try {
-            final Bootstrap b = new Bootstrap();
-            b.group(group).channel(NioDatagramChannel.class)
-                    .option(ChannelOption.SO_BROADCAST, true)
-                    .handler(new ChannelInitializer<NioDatagramChannel>() {
-                @Override
-                public void initChannel(final NioDatagramChannel ch) throws Exception {
-
-                    ChannelPipeline pipeline = ch.pipeline();
-                    pipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(67108864, 0, 4, 0, 4));
-
-            		// decoder must be first
-            		pipeline.addLast("protobufDecoder", new ProtobufDecoder(Route.getDefaultInstance()));
-            		pipeline.addLast("frameEncoder", new LengthFieldPrepender(4));
-            		pipeline.addLast("protobufEncoder", new ProtobufEncoder());
-
-
-            		// our server processor (new instance for each connection)
-            		pipeline.addLast("handler", new UdpServerHandler(null));
-                }
-            });
-
-            // Bind and start to accept incoming connections.
-            Integer pPort = port;
-            InetAddress address  = InetAddress.getLocalHost();
-            System.out.printf("waiting for message %s %s",String.format(pPort.toString()),String.format( address.toString()));
-            b.bind(address,port).sync().channel().closeFuture().await();
-
-        } finally {
-        System.out.print("In Server Finally");
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-        int port =8888;
-        new UdpServer(port).run();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioDatagramChannel.class)
+                    .option(ChannelOption.SO_BROADCAST, true)
+                    .handler(new ChannelInitializer<DatagramChannel>(){
+                        @Override
+                        public void initChannel(DatagramChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                             pipeline.addLast("protobufDecoder", new MyDatagramPacketDecoder(new ProtobufDecoder(NetworkDiscoveryPacket.getDefaultInstance())));
+                             pipeline.addLast("protobufEncoder", new ProtobufEncoder());
+                             pipeline.addLast("handler", new UdpServerHandler());
+
+                        }});
+
+
+
+
+            b.bind(PORT).sync().channel().closeFuture().await();
+        } finally {
+            group.shutdownGracefully();
+        }
     }
 }
