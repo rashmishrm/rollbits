8c3fd12b6764883fe78e6dcf7f8bdc533a8f36ff
Dhrumil1808 <dhrml.shah@gmail.com>
2017-11-05 13:41:20 -0800

    Netty Message

--- src/main/java/com/sjsu/rollbits/datasync/server/resources/NettyMessage.java
+++ src/main/java/com/sjsu/rollbits/datasync/server/resources/NettyMessage.java
@@ -1,7 +1,40 @@
 package com.sjsu.rollbits.datasync.server.resources;
 
+import routing.Pipe;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by dhrumil on 11/5/17.
  */
-public class NettyMessage {
+public class NettyMessage implements NettyInterface {
+
+    Map<String, String> nodeMap = new HashMap<>();
+    @Override
+    public void sendMessageToNode(String senderNodeId, Pipe.Route build) {
+
+        for(Map.Entry<String, String> iter : nodeMap.entrySet()) {
+            String key = iter.getKey();
+            String value = iter.getValue();
+                if(value.equals(senderNodeId))
+                {
+                    Pipe.Route.Path path = build.getPath();
+                    if (path.equals("/message")) {
+                        new MessageResource().process(build);
+                    }
+
+                }
+
+
+        }
+
+
+    }
+
+    @Override
+    public void broadcast() {
+
+
+    }
 }
