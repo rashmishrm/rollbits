94c757c4ae9db08cffc8749f8771f6a7313c2251
Akansha Mehta <akansha.mehta@sjsu.edu>
2017-11-05 13:43:02 -0800

    Update Node.java

--- src/main/java/com/sjsu/rollbits/discovery/Node.java
+++ src/main/java/com/sjsu/rollbits/discovery/Node.java
@@ -1,11 +1,33 @@
 package com.sjsu.rollbits.discovery;
-
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
 
 public class Node {
 
     private String nodeId;
-	
+    private String nodeIp;
+    private int port;
+   
 	public Node(String id){
 	  	nodeId = id;
 	} 
+	
+	public String getNodeIp(){
+		try {
+			nodeIp = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return nodeIp;
+		
+	}
+	
+	public int Getport(){
+		port = UdpClient.PORT;
+		return port;
+	}
+	
+	
 }
