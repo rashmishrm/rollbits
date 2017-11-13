e3dfc682da71b106e0df125e27ed4f98c97f734c
Dhrumil1808 <dhrml.shah@gmail.com>
2017-11-13 11:09:51 -0800

    MainApp updated

--- src/main/java/com/sjsu/rollbits/dao/interfaces/MainApp.java
+++ src/main/java/com/sjsu/rollbits/dao/interfaces/MainApp.java
@@ -5,21 +5,30 @@ import com.sjsu.rollbits.dao.interfaces.service.MessageService;
 import com.sjsu.rollbits.dao.interfaces.service.Service;
 import com.sjsu.rollbits.datasync.server.MessageServer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainApp {
     public static void main(String[] args) {
         MessageService service = new MessageService();
-        
-      
-        
-        Message m= new Message();
-        m.setFromuserid("rashmishrm");
-        m.setTogroupid("ddd");
-        m.setMessage("this is message");
-        service.persist(m);
+       Message m= new Message();
+        //m.setFromuserid("dhrumil");
+        //m.setTogroupid("ddd");
+        ///m.setMessage("this should work");
+        //m.setTouserid("nov12user");
+        //service.persist(m);
        
+       
+       ArrayList<Message> msg=(ArrayList<Message>) MessageService.findAllforuname("nov12user");
+       ArrayList<Message> msg_from=(ArrayList<Message>) MessageService.findAllfromuname("dhrumil");
+        
+        
+        for(int i=0;i<msg.size();i++)
+        {
+        	System.out.println(msg.get(i).getMessage());
+        }
+        
         System.exit(0);
 
     }
