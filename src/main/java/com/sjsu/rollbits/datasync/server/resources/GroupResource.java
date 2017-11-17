565a34af1ee1bd9e5aec758f0a06a8a904e9c0c7
Rashmi Sharma <rashmishrm74@gmail.com>
2017-11-17 14:44:59 -0800

    fixing compilation error

--- src/main/java/com/sjsu/rollbits/datasync/server/resources/GroupResource.java
+++ src/main/java/com/sjsu/rollbits/datasync/server/resources/GroupResource.java
@@ -1,8 +1,10 @@
 package com.sjsu.rollbits.datasync.server.resources;
 
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.sjsu.rollbits.dao.interfaces.model.Group;
 import com.sjsu.rollbits.dao.interfaces.model.GroupUser;
 import com.sjsu.rollbits.dao.interfaces.service.GroupService;
@@ -12,6 +14,7 @@ import com.sjsu.rollbits.sharding.hashing.Message;
 import com.sjsu.rollbits.sharding.hashing.RNode;
 import com.sjsu.rollbits.sharding.hashing.ShardingService;
 
+import io.netty.channel.Channel;
 import routing.Pipe;
 import routing.Pipe.Route;
 
